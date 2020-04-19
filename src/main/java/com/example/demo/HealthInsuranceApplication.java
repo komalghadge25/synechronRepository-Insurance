e52c7e808b495a91dac48644006d7d7dfd0fa439
komalghadge25 <49479361+komalghadge25@users.noreply.github.com>
2020-04-19 17:19:57 +0530

    Update HealthInsuranceApplication.java

--- src/main/java/com/example/demo/HealthInsuranceApplication.java
+++ src/main/java/com/example/demo/HealthInsuranceApplication.java
@@ -1,8 +1,15 @@
 package com.example.demo;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -10,6 +17,19 @@ public class HealthInsuranceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HealthInsuranceApplication.class, args);
+		
 	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/plans").allowedOrigins("http://localhost:8080");
+			}
+		};
+	}
+	
+	
+	
 
 }
